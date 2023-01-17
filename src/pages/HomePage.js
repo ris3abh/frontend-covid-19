3cf63414eb4d3504829876fca4c272af1b38333e
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2023-01-16 19:20:30 -0500

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Banner from '../components/Banner';
-import { makeStyles } from '@material-ui/core';
 
 
 class HomePage extends Component {
